52b645b7a37e378a86856b223446f5b23af92c96
Brian Chen <brianchen8990@gmail.com>
2012-03-06 15:37:33 +0100

    init android version of sencha test

---
+++ src/no/nlink/senchaTest/SenchaTestActivity.java
@@ -0,0 +1,13 @@
+package no.nlink.senchaTest;
+
+import com.phonegap.*;
+import android.os.Bundle;
+
+public class SenchaTestActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
